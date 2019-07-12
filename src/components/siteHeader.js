2507fcf27736bcdb60e9c45e71eb36d789ca31b7
Daniel Noyola <daniel.noyola.dev@gmail.com>
2019-07-11 22:03:28 -0600

    Added extra components

---
+++ src/components/siteHeader.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const SiteHeader = () => (
+  <header id="header">
+    <a href="index.html" className="logo">
+      Massively
+    </a>
+  </header>
+)
+
+export default SiteHeader
