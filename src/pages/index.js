768dbfadc20649bc64f1959ec8144aad83eb7f3a
Daniel Noyola <daniel.noyola.dev@gmail.com>
2019-07-11 21:39:49 -0600

    removed extra pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,11 @@ import React from "react"
 
 const IndexPage = () => (
   <div>
-    <h1>Hola DrupalCamp CR</h1>
+    <h1> Hola DrupalCamp CR </h1>
     <p>
       Drupal Camp Costa Rica es un evento de 2 días sobre la web, en el que
       aprendemos sobre uno de los proyectos de código abierto más grandes:
-      Drupal. Este Camp es uno de los más grandes de América Latina y el más
+      Drupal.Este Camp es uno de los más grandes de América Latina y el más
       constante de la región.
     </p>
   </div>
